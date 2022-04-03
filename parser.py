466984e7b8b5e02f17f8eb71762faa2591ff39db
Konstantin Korepanov <k.korepanov@etpgpb.ru>
2022-04-03 12:27:51 +0300

    some fixes after review

--- parser.py
+++ parser.py
@@ -6,14 +6,19 @@ from datetime import datetime
 file = open(f"{datetime.now().strftime('%d-%m-%Y-%H-%M')}-scan.txt", "w")
 file_err = open("stderr.txt", "w")
 
-# get_cpu_used = ["wsl", "top", "-b", "-n", "1", "|", "head", "-2", "|", "awk", "/^CPU/ {print \$2}"]
 file.write("Отчёт о состоянии системы:\n")
 print("Отчёт о состоянии системы:")
 
-list_users = run(["wsl", "ps", "-o", "user"], stdout=PIPE, stderr=file_err)
-users = list_users.stdout.decode("utf-8").replace("\n", " ").split()[1:]
+command = run(["ubuntu", "run", "ps", "aux"], stdout=PIPE, stderr=file_err, shell=False)
+processes = command.stdout.decode("utf-8").split('\n')
+temp = []
+
+for i in range(len(processes) - 1):
+    temp.append(processes[i].split())
+mas_processes = list(map(list, zip(*temp)))
+
 dict_user = {}
-for i in users:
+for i in mas_processes[0][1:]:
     if i not in dict_user:
         dict_user[i] = 0
 
@@ -21,46 +26,47 @@ users_list = str(list(dict_user.keys())).replace("[", "").replace("]", "")
 print("Пользователи системы:", users_list)
 file.write("Пользователи системы: " + users_list)
 
-print("Процессов запущено:", len(users))
-file.write("\nПроцессов запущено: " + str(len(users)) + "\n")
+print("Процессов запущено:", len(mas_processes[0]) - 1)
+file.write(f"\nПроцессов запущено: {len(mas_processes[0]) - 1}\n")
 
 print("Пользовательских процессов:")
 file.write("Пользовательских процессов:\n")
 
-for i in users:
+for i in mas_processes[0][1:]:
     if i in dict_user.keys():
         dict_user[i] += 1
 user_processes = str(dict_user).replace("{", "").replace("}", "").replace(", ", "\n")
 print(user_processes)
 file.write(user_processes + "\n")
 
-process = run(["wsl", "free", "-m", "|", "awk", "/^Mem/ {print \$3}"], stdout=PIPE, stderr=file_err)
-used_memory = int(process.stdout.decode("utf-8"))
-process = run(["wsl", "free", "-m", "|", "awk", "/^Mem/ {print \$2}", "|", "head", "-2", "|", "tail", "-1"],
-              stdout=PIPE, stderr=file_err)
-total_memory = int(process.stdout.decode("utf-8"))
-print("Всего памяти используется: " + str(round(used_memory * 100 / total_memory, 1)).replace("\n", "") + "%")
-file.write("Всего памяти используется: " + str(round(used_memory * 100 / total_memory, 1)).replace("\n", "") + "%\n")
+memory = 0.0
+max_memory = 0.0
+max_memory_process = ''
+for i in range(1, len(mas_processes[3])):
+    memory += float(mas_processes[3][i])
+    if max_memory <= float(mas_processes[3][i]):
+        max_memory = float(mas_processes[3][i])
+        max_memory_process = mas_processes[10][i]
 
-process = run(["wsl", "top", "-b", "-n", "1", "|", "head", "-2", "|", "awk", "/^CPU/ {print \$2}"], stdout=PIPE,
-              stderr=file_err)
+print(f"Всего памяти используется: {memory}%")
+file.write(f"Всего памяти используется: {memory}%\n")
 
-cpu = process.stdout.decode("utf-8")
-print("Всего CPU используется: " + cpu.replace("\n", ""))
-file.write("Всего CPU используется: " + cpu)
+cpu = 0.0
+max_cpu = 0.0
+max_cpu_process = ''
+for i in range(1, len(mas_processes[2])):
+    cpu += float(mas_processes[2][i])
+    if max_cpu <= float(mas_processes[3][i]):
+        max_cpu = float(mas_processes[3][i])
+        max_cpu_process = mas_processes[10][i]
+print(f"Всего CPU используется: {cpu}%")
+file.write(f"Всего CPU используется: {cpu}%\n")
 
-process = run(
-    ["wsl", "ps", "-e", "-o", "comm=", "|", "sort", "-n", "-r", "-k", "1", "|", "head", "-1", "|", "cut", "-c1-20"],
-    stdout=PIPE, stderr=file_err)
-max_memory = process.stdout.decode("utf-8")
-print("Больше всего памяти использует: " + max_memory.replace("\n", ""))
-file.write("Больше всего памяти использует: " + max_memory)
+print(f"Больше всего памяти использует: {max_memory}% {max_memory_process[:20]}")
+file.write(f"Больше всего памяти использует: {max_memory}% {max_memory_process[:20]}\n")
 
-process = run(["wsl", "top", "-b", "-n", "1", "|", "tail", "-2", "|", "awk", "{print \$8, \$9}"],
-              stdout=PIPE, stderr=file_err)
-max_cpu = process.stdout.decode("utf-8").split()
-print("Больше всего CPU использует: " + max_cpu[0] + " " + max_cpu[1][0:20])
-file.write("Больше всего CPU использует: " + max_cpu[0] + " " + max_cpu[1][0:20])
+print(f"Больше всего CPU использует: {max_cpu}% {max_cpu_process[:20]}")
+file.write(f"Больше всего CPU использует: {max_cpu}% {max_cpu_process[:20]}")
 
 file.close()
 file_err.close()
